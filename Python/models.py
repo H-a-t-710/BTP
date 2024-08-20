232cfdf43296340620724d56622ec98b6d276f0e
Deepak Kumar <105198447+Deepak-Kumar-1764@users.noreply.github.com>
2024-08-20 05:52:09 +0530

    Update models.py

--- Python/models.py
+++ Python/models.py
@@ -3,19 +3,20 @@ from app import db
 from datetime import datetime
 from sqlalchemy.orm import relationship
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+class User(db.Model): 
+    __tablename__ = 'user_detail'
+    id = db.Column(db.Integer, primary_key=True) 
     name = db.Column(db.String(25), unique=False, nullable=False)
     username = db.Column(db.String(25), unique=True, nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
     password_hash = db.Column(db.String(150), nullable=False)
-    data_tables = relationship('Data_Table', back_populates='user')
+    data_tables = relationship('Data_Table', back_populates='user')  #for the foreign_key
 
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password)   #hashing password
 
     def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+        return check_password_hash(self.password_hash, password)  
 
 class Data_Table(db.Model):
     __tablename__ = 'data_table'
@@ -23,7 +24,6 @@ class Data_Table(db.Model):
     user_name = db.Column(db.String(25), db.ForeignKey('user.username'), nullable=False)
     stored_data = db.Column(db.Text, nullable=False)
     time = db.Column(db.DateTime, default=datetime.utcnow)
-   
-
+    
     # Define the relationship
     user = relationship('User', back_populates='data_tables')

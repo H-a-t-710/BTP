b1016c782041e65eb5d31c6d66fe0243e8e51c7c
Deepak Kumar <105198447+Deepak-Kumar-1764@users.noreply.github.com>
2024-08-20 05:48:38 +0530

    Update app.py

--- Python/app.py
+++ Python/app.py
@@ -1,22 +1,25 @@
 #Imports
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask  
+from flask_sqlalchemy import SQLAlchemy   
 from flask_migrate import Migrate
-db = SQLAlchemy()
+from routes import register_routes  
+
+#creating database object 
+db = SQLAlchemy() 
 
 #My app
 def create_app():
-    app = Flask(__name__)
-    app.secret_key = "Deepak@Hemant"
+    app = Flask(__name__)   #creating flask instance
+    app.secret_key = "Deepak@Hemant"   #Flask's Built-in Security
 
     #Configure SQL Alchemy
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
-    app.config["SQLALCHEMY_TRACK_MODIFICATONS"] = False
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"  #configur of database location 
+    app.config["SQLALCHEMY_TRACK_MODIFICATONS"] = False  
     
-    db.init_app(app)
-    from routes import register_routes
-    register_routes(app)
-    migrate = Migrate(app,db)
+    db.init_app(app)    #databse intialisation to link app
+  
+    register_routes(app)  #get and post on the routes
+    migrate = Migrate(app,db)  #migration of database
 
     return app
    
